c3c4b133ede21052ec0e01eacc69bd7f28ee8d69
Rajesh mule <rajeshsmule@gmail.com>
2020-02-11 16:38:04 +0530

    add error handdler

--- node_day-5/session-code/app.js
+++ node_day-5/session-code/app.js
@@ -6,12 +6,6 @@ const User = require('./models/User');
 const port = process.env.PORT || 5000;
 const mongodbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/day5';
 
-const app = express();
-
-//middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 //connect mongodb 
 mongoose.connect(mongodbUrl, {
     useCreateIndex: true,
@@ -24,64 +18,93 @@ mongoose.connect(mongodbUrl, {
     console.log("mongodb is connected");
 }).catch(err => console.log(err.reason));
 
+// inst
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routers
+app.get('/', (req, res, next) =>
+{
+    // console.log(req.xhr);
+    //Create an error and pass it to the next function
+    var err = new Error("Something went wrong");
+    next(err);
+});
 // get all users
-app.get('/users', (req, res) =>
+app.get('/users', (req, res, next) =>
 {
     User.find({}, (err, users) =>
     {
-        if (err) return res.send(err);
+        if (err) return next(err);
         res.json({ users });
     });
 });
 // create user
-app.post('/users', (req, res) =>
+app.post('/users', (req, res, next) =>
 {
     const data = req.body;
     User.create(data, (err, createdUser) =>
     {
-        if (err) return res.send(err);
+        if (err) return next(err);
         // res.json(createdUser);
         User.find({}, (err, users) =>
         {
-            if (err) return res.send(err);
+            if (err) return next(err);
             res.json({ users });
         });
     });
 });
 // find by id 
-app.get('/users/:id', (req, res) =>
+app.get('/users/:id', (req, res, next) =>
 {
     const id = req.params.id;
     User.findById(id, (err, findUser) =>
     {
-        if (err) return res.send(err);
+        if (err) return next(err);
         res.json(findUser);
     });
 });
 // delete one user 
-app.delete('/users/:id', (req, res) =>
+app.delete('/users/:id', (req, res, next) =>
 {
     const id = req.params.id;
     User.findByIdAndDelete(id, (err, deletedUser) =>
     {
-        if (err) return res.send(err);
+        if (err) return next(err);
         console.log('User successfully removed.');
         res.json(deletedUser);
     })
 });
 // update user
-app.put('/users/:id', (req, res) =>
+app.put('/users/:id', (req, res, next) =>
 {
     const id = req.params.id;
     const updateData = req.body;
 
     User.findByIdAndUpdate(id, updateData, (err, user) =>
     {
-        if (err) return res.send(err);
+        if (err) return next(err);
         console.log('user is updated');
         res.json("uesr is updated");
 
     });
+});
+
+app.use((req, res, next) =>
+{
+    res.status(404);
+    res.send("Page not found");
+});
+
+
+//An error handling middleware
+app.use((err, req, res, next) =>
+{
+    res.status(500);
+    res.send("Oops, something went wrong.")
 });
 
 
