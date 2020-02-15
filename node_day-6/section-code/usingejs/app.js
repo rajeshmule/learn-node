4553718e407735448977175666ffd9ba47d9c6ff
Rajesh mule <rajeshsmule@gmail.com>
2020-02-15 15:39:25 +0530

    add ejs pages in view in usingejs project

--- node_day-6/section-code/usingejs/app.js
+++ node_day-6/section-code/usingejs/app.js
@@ -45,6 +45,7 @@ app.use((req, res, next) =>
 //An error handling middleware
 app.use((err, req, res, next) =>
 {
+    console.log(err);
     res.status(500);
     res.send("Oops, something went wrong.")
 });
